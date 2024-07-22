e473bad187a2c57c64ef7087821fcb1a3b815236
Harshil Tomar <harshil.tomar@xarterian.com>
2024-07-22 17:00:46 +0530

    feat: image fix

--- src/components/micro/card-match-series-info.tsx
+++ src/components/micro/card-match-series-info.tsx
@@ -1,29 +1,18 @@
+import Live from "../../../assets/images/live.png"
+
 interface MatchSeriesInfoProps {
     currentMatch: any;
 }
 
-
-export default function MatchSeriesInfo({currentMatch}: MatchSeriesInfoProps) {
-  return (
-    <>
-        {/* <text className='text-red-600 ml-2'>🔴 {currentMatch?.sLiveGameStatusStr && currentMatch.sLiveGameStatusStr.charAt(0).toUpperCase() + currentMatch.sLiveGameStatusStr.slice(1)}</text>
-        <text className='text-gray-500 ml-2'>⚪️ {currentMatch.sSubtitle}</text>
-        <text className='text-gray-500 ml-2'>
-        ⚪️ {currentMatch?.oSeries?.sTitle && 
-            (currentMatch.oSeries.sTitle.length > 26 
-                ? `${currentMatch.oSeries.sTitle.slice(0, 26)}..` 
-                : currentMatch.oSeries.sTitle
-            )
-        }
-        </text> */}
-        <div className="flex">
-            <div className="flex bg-gradient-to-r from-[#dad6f4] to-white p-2 dark:bg-gradient-to-r dark:from-slate-500 dark:to-[#121110]">
-                <text className='text-[#252775] dark:text-[#ABABAB] ml-2'>
+export default function MatchSeriesInfo({ currentMatch }: MatchSeriesInfoProps) {
+    return (
+        <div className="flex justify-between items-center">
+            <div className=" bg-gradient-to-r from-[#dad6f4] to-white p-2 dark:bg-gradient-to-r dark:from-slate-500 dark:to-[#121110]">
+                <text className='text-[#252775] dark:text-[#ABABAB]'>
                     {currentMatch?.oSeries?.sTitle}
                 </text>
             </div>
-            <img src="/src/assets/live.png" className="pl-auto mt-1 h-5" alt="live"/>
+            <img src={Live} className="h-5" alt="live" />
         </div>
-    </>
-  )
+    );
 }
--- src/components/shared/brand-logo.tsx
+++ src/components/shared/brand-logo.tsx
@@ -1,7 +1,9 @@
+import OtnLogo from "../../../assets/images/otnlogo.png";
+
 export default function BrandLogo() {
   return (
     <a href="https://www.oneturf.news/" target="_blank">
-      <img className='m-2' src='/src/assets/otnlogo.png' width={200} alt="Logo"/>
+      <img className='m-2' src={OtnLogo} width={200} alt="Logo"/>
     </a>
   )
 }
