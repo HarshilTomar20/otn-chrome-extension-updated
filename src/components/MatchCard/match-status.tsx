91df47b91f25e6d7630ae9b4a343b84e6118283e
Harshil Tomar <harshil.tomar@xarterian.com>
2024-07-23 16:31:18 +0530

    bug: match status fix

--- src/components/MatchCard/match-status.tsx
+++ src/components/MatchCard/match-status.tsx
@@ -6,7 +6,7 @@ export default function MatchStatus({ currentMatch }: TossResultProps) {
   return (
     <div className='my-4 text-center border-t border-b border-[#5E5E5E99] p-2' style={{ borderTopWidth: '0.5px', borderBottomWidth: '0.5px' }}>
       <span className='text-[#DA3400] text-left'>
-        {currentMatch?.sStatusNote ? currentMatch?.sStatusNote : currentMatch?.oToss?.sText}
+        {currentMatch?.sStatusNote ? currentMatch?.sStatusNote : currentMatch?.oToss?.sText || "Match Will Start Soon..."}
       </span>
     </div>
   );
