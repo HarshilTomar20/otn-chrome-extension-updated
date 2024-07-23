48670cafc7981448421d0840469070d505269b81
Harshil Tomar <harshil.tomar@xarterian.com>
2024-07-23 14:36:00 +0530

    fix: scroll fix

--- src/components/micro/cardscroll-left.tsx
+++ src/components/micro/cardscroll-left.tsx
@@ -10,14 +10,14 @@ interface ToggleProps {
 export default function ScrollerLeft({goToPreviousMatch, matches, theme}: ToggleProps) {
   return (
     <div className='flex justify-between'>
-          <button 
-            className="font-bold px-4 py-2 rounded-md text-[#252775] dark:text-orange-600 text-3xl leading-none"
-            onClick={goToPreviousMatch}
-            disabled={matches.length === 0}
-          >
-            <img className="h-5 w-5" src={theme === "light" ? LeftLight : LeftDark}/>
-          </button>
-      </div>
+      <button 
+        className="font-bold px-4 py-2 rounded-md text-[#252775] dark:text-orange-600 text-3xl leading-none"
+        onClick={goToPreviousMatch}
+        disabled={matches.length === 0}
+      >
+        <img className="h-5 w-5" src={theme === "light" ? LeftLight : LeftDark}/>
+      </button>
+    </div>
   )
 }
 
--- src/components/micro/cardscroll-right.tsx
+++ src/components/micro/cardscroll-right.tsx
@@ -10,14 +10,14 @@ interface ToggleProps {
 export default function ScrollerRight({matches, goToNextMatch, theme}: ToggleProps) {
   return (
     <div className='flex justify-between'>
-          <button 
-            className="font-bold px-4 py-2 rounded-md text-[#252775] dark:text-orange-600 text-3xl leading-none"
-            onClick={goToNextMatch}
-            disabled={matches.length === 0}
-          >
-            <img className="h-5 w-5" src={theme === "light" ? RightLight : RightDark}/>
-          </button>
-      </div>
+      <button 
+        className="font-bold px-4 py-2 rounded-md text-[#252775] dark:text-orange-600 text-3xl leading-none"
+        onClick={goToNextMatch}
+        disabled={matches.length === 0}
+      >
+        <img className="h-5 w-5" src={theme === "light" ? RightLight : RightDark}/>
+      </button>
+    </div>
   )
 }
 
